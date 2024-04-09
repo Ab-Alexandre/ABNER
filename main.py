dcc040fdb742b9a163169ed30c3f1add1fb4ac21
Ab-Alexandre <aluno.sesipaulista@SISTEMAFIEPE.LOCAL>
2024-04-09 08:03:30 -0300

    macaco

---
+++ main.py
@@ -0,0 +1,16 @@
+class alunos:
+    def __init__(self, matricula, nome, sexo, idade):
+
+        self.matricula = matricula
+        self.nome = nome
+        self.sexo = sexo
+        self.idade = idade
+
+if __name__ == "__main__":
+    aluno1 = alunos("134", "Abner", "m", "17")
+    aluno2 = alunos("135", "Cleiton", "m", "18")
+    aluno3 = alunos("136", "Rasta", "f", "16")
+
+print (aluno1.matricula, aluno1.nome, aluno1.sexo, aluno1.idade)
+print (aluno2.matricula, aluno2.nome, aluno2.sexo, aluno2.idade)
+print (aluno3.matricula, aluno3.nome, aluno3.sexo, aluno3.idade)
